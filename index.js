a6d9f49dfb67b3e000fc17e58e2fb544924b5bf4
oss6 <ossedb@gmail.com>
2015-07-05 20:17:57 +0100

    Added calls

--- index.js
+++ index.js
@@ -2,6 +2,40 @@
 
 var ghGot = require('gh-got');
 
+var get_followers = function (username, token, callback) {
+    ghGot('users/' + username + '/followers', { token: token }, callback);
+};
+
+var getResults = function (res1, res2, cb) {
+    if (res1.err) {
+        cb(res1.err);
+        return;
+    }
+
+    if (res2.err) {
+        cb(res2.err);
+        return;
+    }
+
+    var followers1 = res1.data.items.map(function (user) {
+        return user.login;
+    });
+
+    var followers2 = res2.data.items.map(function (user) {
+        return user.login;
+    });
+
+    return [followers1, followers2];
+};
+
+function intersect(a, b) {
+    var t;
+    if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+    return a.filter(function (e) {
+        if (b.indexOf(e) !== -1) return true;
+    });
+}
+
 module.exports = function (username1, username2, token, cb) {
 
     if (typeof username1 !== 'string' || typeof username2 !== 'string') {
@@ -13,19 +47,16 @@ module.exports = function (username1, username2, token, cb) {
         token = null;
     }
 
-    var followers1 = [],
-        followers2 = [];
+    var result1, result2;
 
-    ghGot('users/' + username1 + '/followers', {
+    get_followers(username1, token, function (err, data) {
+        result1 = { err: err, data: data };
 
-        token: token
+        get_followers(username2, token, function (err, data) {
+            result2 = { err: err, data: data };
 
-    }, function (err, data) {
-        if (err) {
-            cb(err);
-            return;
-        }
-
-        console.log(data);
+            var followers = intersect.apply(null, getResults(result1, result2, cb));
+            cb(null, followers);
+        });
     });
 };
